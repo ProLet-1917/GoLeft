f8620232d8b9387997514778ab8b9273692a424a
ProLet <2891163038@qq.com>
2020-09-23 19:04:49 +0800

    no message

--- GoLeft/Assets/Player.cs
+++ GoLeft/Assets/Player.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [Header("Player Variables")]
+
+    public float speedMove = 0.001f;
+    public float forceJump = 0.001f;
+
+    [HideInInspector]
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +22,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        float inputHorizonal = Input.GetAxis("Horizontal");
+        float inputVertical = Input.GetAxis("Vertical");
+        var xPos = inputHorizonal * speedMove;
+        var yPos = inputVertical * forceJump;
+
+        transform.position += new Vector3(xPos, yPos, 0);
+        transform.localScale = new Vector2(inputHorizonal < 0 ? -1 : 1, transform.localScale.y);
     }
 }
