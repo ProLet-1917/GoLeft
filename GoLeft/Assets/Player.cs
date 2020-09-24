dfdf812ffc8a6e51be2a5b679ce7ff01ac295d1c
ProLet <2891163038@qq.com>
2020-09-24 19:42:14 +0800

    实现运动、跳跃

--- GoLeft/Assets/Player.cs
+++ GoLeft/Assets/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("Player Variables")]
+    [Header("Public Variables")]
 
     public float speedMove = 0.001f;
     public float forceJump = 0.001f;
 
-    [HideInInspector]
+    [Header("Private Variables")]
 
-    
+    [SerializeField]
+    private float inputHorizontal = 0.0f;
+    [SerializeField]
+    private bool jumpAble = false;
 
+    private Rigidbody2D rb2d;
+    private Collider2D col;
+
+    private void Awake()
+    {
+        rb2d = gameObject.AddComponent<Rigidbody2D>();
+        col = gameObject.AddComponent<BoxCollider2D>();
+    }
     // Start is called before the first frame update
     void Start()
     {
@@ -22,12 +33,40 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float inputHorizonal = Input.GetAxis("Horizontal");
-        float inputVertical = Input.GetAxis("Vertical");
-        var xPos = inputHorizonal * speedMove;
-        var yPos = inputVertical * forceJump;
+        inputHorizontal = Input.GetAxis("Horizontal");
+        // inputVertical = Input.GetAxis("Vertical");
+        
+        //var xPos = inputHorizontal * speedMove;
+        //var yPos = inputVertical * forceJump;
 
-        transform.position += new Vector3(xPos, yPos, 0);
-        transform.localScale = new Vector2(inputHorizonal < 0 ? -1 : 1, transform.localScale.y);
+        //move
+        transform.Translate(Vector2.right * inputHorizontal * speedMove * Time.deltaTime);
+        //jump twice
+        if(Input.GetAxis("Jump") > 0 && jumpAble)
+        {
+            rb2d.AddForce(transform.up * forceJump);
+            jumpAble = false;
+           
+        }
+       
+        //turn round
+        if(inputHorizontal < 0)
+        {
+            transform.localScale = new Vector2(-1, transform.localScale.y);
+        }
+        if(inputHorizontal > 0)
+        {
+            transform.localScale = new Vector2(1, transform.localScale.y);
+        }
     }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //jumpCount clear
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            Debug.Log("HitGround");
+            jumpAble = true;
+        }
+    }
+
 }
